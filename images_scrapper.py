b60fe2cda8b259f033518695fccf9e1513f0365c
Nikolay Shcheglov <ndnd@mail.ru>
2017-11-01 20:31:16 +0300

    All cards info exports to json file

--- images_scrapper.py
+++ images_scrapper.py
@@ -1,17 +1,15 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
-MAIN_URL = 'https://magiccards.info/'
 SITEMAP_URL = 'https://magiccards.info/sitemap.html'
-EDITION_TPL = 'https://magiccards.info/{}/en.html'
-CARD_TPL = 'https://magiccards.info/{}/en/{}.html'
+EDITION_URL_TPL = 'https://magiccards.info/{}/en.html'
+CARD_IMG_URL_TPL = 'https://magiccards.info/scans/en/{}/{}.jpg'
 
 
-# TODO lists to tuples?
-
-def collect_all_editions(sitemap_url):
+def collect_all_editions():
     editions_list = []
-    response = requests.get(sitemap_url)
+    response = requests.get(SITEMAP_URL)
     soup = BeautifulSoup(response.text, 'html.parser')
     small_tags_list = soup.find_all('table')[1].find_all('small')
     for t in small_tags_list:
@@ -19,52 +17,50 @@ def collect_all_editions(sitemap_url):
     return editions_list
 
 
-def collect_edition_cards_numbers(edition_url):
-    cards_numbers_list = []
+def collect_edition_cards_info(edition):
+    edition_cards = []
+    edition_url = EDITION_URL_TPL.format(edition)
     response = requests.get(edition_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    table_rows_list = soup.find_all('table')[3].find_all('tr')
+    table_rows_list = soup.find('table', cellpadding='3').find_all('tr')
     table_rows_list.pop(0)
     for r in table_rows_list:
         card_number = r.find('td').text
-        cards_numbers_list.append(card_number)
-    return cards_numbers_list
+        card_name = r.find_all('td')[1].find('a').text
+        edition_cards.append({'card_name': card_name, 'card_number': card_number, 'edition': edition})
+    return edition_cards
 
 
 def cards_numbers_to_editions():
-    result = {}
-    editions = collect_all_editions(SITEMAP_URL)
+    cards = []
+    editions = collect_all_editions()
     for e in editions:
-        edition_url = EDITION_TPL.format(e)
-        edition_cards = collect_edition_cards_numbers(edition_url)
-        result.update({e: edition_cards})
+        edition_cards = collect_edition_cards_info(e)
+        cards.extend(edition_cards)
         print('{} edition complete'.format(e))
-        break  # delete this after tests
-    return result
+    return cards
 
 
-def get_images_urls():
-    urls = []
-    editions_cards = cards_numbers_to_editions()
-    for edition in editions_cards.keys():
-        for card_number in editions_cards[edition]:
-            card_image_url = CARD_TPL.format(edition, card_number)
-            urls.append(card_image_url)
-    return urls
+def get_cards_list():
+    cards = cards_numbers_to_editions()
+    for card in cards:
+        card_image_url = CARD_IMG_URL_TPL.format(card['edition'], card['card_number'])
+        card.update({'card_image_url': card_image_url})
+    return cards
 
 
-def images_urls_to_file():
-    urls_list = get_images_urls()
-    with open('urls.txt', 'w') as f:
-        for u in urls_list:
-            f.write(u + '\n')
+def cards_to_json_file():
+    cards_list = get_cards_list()
+    with open('cards.json', 'w') as f:
+        f.write(json.dumps(cards_list, sort_keys=True, indent=4))
     print('Urls exported to file')
 
 
 def main():
-    images_urls_to_file()
+    cards_to_json_file()
 
 
-main()
+if __name__ == '__main__':
+    main()
 
 
