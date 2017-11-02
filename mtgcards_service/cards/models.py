1b4505486cf30a1c2c87d4e679cdf509da26a3bd
Nikolay Shcheglov <ndnd@mail.ru>
2017-11-02 14:06:54 +0300

    Create models for mysql db

--- mtgcards_service/cards/models.py
+++ mtgcards_service/cards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Card(models.Model):
+    card_name = models.CharField(max_length=200)
+    path = models.CharField(max_length=100)
+
+
+class Purchases(models.Model):
+    card = models.OneToOneField(Card, on_delete=models.CASCADE)
+    user = models.OneToOneField(User)
