7f48068876d28907483c6928fca53ca8ba3f7c50
Nikolay Shcheglov <ndnd@mail.ru>
2017-11-03 22:46:03 +0300

    Search works with local and original database now. New cards downloads and saves locally.

--- mtgcards_service/cards/views.py
+++ mtgcards_service/cards/views.py
@@ -1,7 +1,13 @@
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+from tools.card_images_downloader import download_file
 from django.http import HttpResponse
 from django.template import loader
 
 from .models import Card
+
+MAGIC_CARDS_SITE = 'https://magiccards.info'
 
 
 def index(request):
@@ -22,13 +28,43 @@ def list_all(request):
 
 
 def search(request):
+    card_name = ''
+    card_local_path = 'cards/images'
+    is_card_found = False
     name = request.GET.get('name')
-    found_card = Card.objects.filter(name=name).first()
+    card_from_db = Card.objects.filter(name=name).first()
+    if card_from_db:
+        card_name = card_from_db.name
+        card_local_path += card_from_db.local_path
+        is_card_found = True
+    else:
+        response = requests.get('{}/query?q=!{}'.format(MAGIC_CARDS_SITE, name))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        card = soup.find(lambda tag: tag.name == 'img' and name.lower() in tag['alt'].lower())
+
+        if card:
+            card_name = card['alt']
+            card_local_path += card['src']
+            is_card_found = True
+
+            if not Card.objects.filter(name=card['alt']).first():
+                img_url = MAGIC_CARDS_SITE + card['src']
+                try:
+                    download_file(img_url)
+                except urllib3.exceptions.MaxRetryError:
+                    print('cant load image')
+                else:
+                    new_card = Card(
+                        name=card['alt'],
+                        local_path=card['src']
+                    )
+                    new_card.save()
+
     template = loader.get_template('cards/result.html')
-    if found_card:
-        found_card.local_path = 'cards/images' + found_card.local_path
     context = {
-        'card': found_card,
+        'card': {'name': card_name, 'local_path': card_local_path},
+        'searching_name': name,
+        'is_card_found': is_card_found
     }
     return HttpResponse(template.render(context, request))
 
