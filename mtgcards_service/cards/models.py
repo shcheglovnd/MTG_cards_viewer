17aa8401a9c86f9e2efa28fb50b611b0bdcf9946
Nikolay Shcheglov <ndnd@mail.ru>
2017-11-04 16:08:57 +0300

    Introduced a money system

--- mtgcards_service/cards/models.py
+++ mtgcards_service/cards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Card(models.Model):
@@ -11,8 +13,25 @@ class Card(models.Model):
 
 
 class Purchases(models.Model):
-    card = models.ForeignKey(Card, on_delete=models.CASCADE)
-    user = models.ForeignKey(User)
+    query = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.user) + ' - ' + str(self.card)
+        return str(self.user) + ' - ' + str(self.query)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    money = models.PositiveIntegerField(default=1000)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return str(self.user) + ' - ' + str(self.money)
--- mtgcards_service/cards/views.py
+++ mtgcards_service/cards/views.py
@@ -5,10 +5,13 @@ from tools.card_images_downloader import download_file
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Card
+from .models import Purchases
 
 MAGIC_CARDS_SITE_URL = 'https://magiccards.info'
+SEARCH_PRICE = 50
 
 
 def index(request):
@@ -34,34 +37,42 @@ def search(request):
     result_card_local_path = ''
     is_card_found = False
 
+    is_money_enough = request.user.profile.money >= SEARCH_PRICE
     searching_name = request.GET.get('name')
-    card_from_local_db = Card.objects.filter(name=searching_name).first()
 
-    if card_from_local_db:
-        result_card_name = card_from_local_db.name
-        result_card_local_path = card_from_local_db.local_path
-        is_card_found = True
-    else:
-        card_from_site = search_on_magic_cards_site(searching_name)
-        if card_from_site:
-            result_card_name = card_from_site['name']
-            result_card_local_path = card_from_site['local_path']
+    if is_money_enough or is_user_made_this_query(searching_name):
+        card_from_local_db = Card.objects.filter(name=searching_name).first()
+
+        if card_from_local_db:
+            result_card_name = card_from_local_db.name
+            result_card_local_path = card_from_local_db.local_path
             is_card_found = True
+        else:
+            card_from_site = search_on_magic_cards_site(searching_name)
+            if card_from_site:
+                result_card_name = card_from_site['name']
+                result_card_local_path = card_from_site['local_path']
+                is_card_found = True
 
-            if not is_card_in_local_db(result_card_name):
-                card_image_url = MAGIC_CARDS_SITE_URL + result_card_local_path
-                try:
-                    download_file(card_image_url)
-                except urllib3.exceptions.MaxRetryError:
-                    print('Cant download card image')
-                else:
-                    save_card_to_local_db(result_card_name, result_card_local_path)
+                if not is_card_in_local_db(result_card_name):
+                    card_image_url = MAGIC_CARDS_SITE_URL + result_card_local_path
+                    try:
+                        download_file(card_image_url)
+                    except urllib3.exceptions.MaxRetryError:
+                        print('Cant download card image')
+                    else:
+                        save_card_to_local_db(result_card_name, result_card_local_path)
+        if not is_user_made_this_query(searching_name):
+            save_user_query(searching_name, request.user)
+            dec_user_money(request.user.id, SEARCH_PRICE)
+            request.user.profile.money -= SEARCH_PRICE
 
     template = loader.get_template('cards/result.html')
     context = {
         'card': {'name': result_card_name, 'local_path': 'cards/images' + result_card_local_path},
         'searching_name': searching_name,
-        'is_card_found': is_card_found
+        'is_card_found': is_card_found,
+        'is_money_enough': is_money_enough,
     }
     return HttpResponse(template.render(context, request))
 
@@ -87,5 +98,24 @@ def save_card_to_local_db(name, local_path):
         local_path=local_path
     )
     new_card.save()
+
+
+def dec_user_money(user_id, count):
+    user = User.objects.get(id=user_id)
+    user.profile.money -= count
+    user.save()
+
+
+def is_user_made_this_query(query):
+    purchase = Purchases.objects.filter(query=query).first()
+    if purchase:
+        return True
+    else:
+        return False
+
+
+def save_user_query(query, user):
+    purchase = Purchases(query=query, user=user)
+    purchase.save()
 
 
