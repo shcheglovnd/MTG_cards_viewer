a7e2591e8e412ec24e9c8f6755a18c0913d3c97a
Nikolay Shcheglov <ndnd@mail.ru>
2017-11-04 11:41:29 +0300

    Refactoring views

--- mtgcards_service/cards/views.py
+++ mtgcards_service/cards/views.py
@@ -7,7 +7,7 @@ from django.template import loader
 
 from .models import Card
 
-MAGIC_CARDS_SITE = 'https://magiccards.info'
+MAGIC_CARDS_SITE_URL = 'https://magiccards.info'
 
 
 def index(request):
@@ -28,44 +28,62 @@ def list_all(request):
 
 
 def search(request):
-    card_name = ''
-    card_local_path = 'cards/images'
+    result_card_name = ''
+    result_card_local_path = ''
     is_card_found = False
-    name = request.GET.get('name')
-    card_from_db = Card.objects.filter(name=name).first()
-    if card_from_db:
-        card_name = card_from_db.name
-        card_local_path += card_from_db.local_path
+
+    searching_name = request.GET.get('name')
+    card_from_local_db = Card.objects.filter(name=searching_name).first()
+
+    if card_from_local_db:
+        result_card_name = card_from_local_db.name
+        result_card_local_path = card_from_local_db.local_path
         is_card_found = True
     else:
-        response = requests.get('{}/query?q=!{}'.format(MAGIC_CARDS_SITE, name))
-        soup = BeautifulSoup(response.text, 'html.parser')
-        card = soup.find(lambda tag: tag.name == 'img' and name.lower() in tag['alt'].lower())
-
-        if card:
-            card_name = card['alt']
-            card_local_path += card['src']
+        card_from_site = search_on_magic_cards_site(searching_name)
+        if card_from_site:
+            result_card_name = card_from_site['name']
+            result_card_local_path = card_from_site['local_path']
             is_card_found = True
 
-            if not Card.objects.filter(name=card['alt']).first():
-                img_url = MAGIC_CARDS_SITE + card['src']
+            if not is_card_in_local_db(result_card_name):
+                card_image_url = MAGIC_CARDS_SITE_URL + result_card_local_path
                 try:
-                    download_file(img_url)
+                    download_file(card_image_url)
                 except urllib3.exceptions.MaxRetryError:
-                    print('cant load image')
+                    print('Cant download card image')
                 else:
-                    new_card = Card(
-                        name=card['alt'],
-                        local_path=card['src']
-                    )
-                    new_card.save()
+                    save_card_to_local_db(result_card_name, result_card_local_path)
 
     template = loader.get_template('cards/result.html')
     context = {
-        'card': {'name': card_name, 'local_path': card_local_path},
-        'searching_name': name,
+        'card': {'name': result_card_name, 'local_path': 'cards/images' + result_card_local_path},
+        'searching_name': searching_name,
         'is_card_found': is_card_found
     }
     return HttpResponse(template.render(context, request))
+
+
+def search_on_magic_cards_site(name):
+    response = requests.get('{}/query?q=!{}'.format(MAGIC_CARDS_SITE_URL, name))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    card = soup.find(lambda tag: tag.name == 'img' and name.lower() in tag['alt'].lower())
+    if card:
+        return {'name': card['alt'], 'local_path': card['src']}
+
+
+def is_card_in_local_db(name):
+    if Card.objects.filter(name=name).first():
+        return True
+    else:
+        return False
+
+
+def save_card_to_local_db(name, local_path):
+    new_card = Card(
+        name=name,
+        local_path=local_path
+    )
+    new_card.save()
 
 
